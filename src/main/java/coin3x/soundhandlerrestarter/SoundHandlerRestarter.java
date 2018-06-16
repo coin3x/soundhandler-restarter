49f9ed41c94a602daf60e446532bf0b72120d45d
Coin <cce5784@gmail.com>
2018-06-16 15:39:48 +0800

    Update to MC 1.12.2

--- src/main/java/coin3x/soundhandlerrestarter/SoundHandlerRestarter.java
+++ src/main/java/coin3x/soundhandlerrestarter/SoundHandlerRestarter.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = SoundHandlerRestarter.MODID, version = SoundHandlerRestarter.VERSION)
 public class SoundHandlerRestarter {
     public static final String MODID = "shrestarter";
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.1.2";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/coin3x/soundhandlerrestarter/client/GuiEventHandler.java
+++ src/main/java/coin3x/soundhandlerrestarter/client/GuiEventHandler.java
@@ -2,6 +2,7 @@ package coin3x.soundhandlerrestarter.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiScreenOptionsSounds;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -9,14 +10,16 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class GuiEventHandler {
     @SubscribeEvent
     public void onPostInitGui(GuiScreenEvent.InitGuiEvent.Post event) {
-        if (event.gui instanceof GuiScreenOptionsSounds) {
-            event.buttonList.add(new GuiButton(888, event.gui.width / 2 - 100, event.gui.height / 6 + 130, "Restart SoundHandler"));
+        GuiScreen gui = event.getGui();
+
+        if (gui instanceof GuiScreenOptionsSounds) {
+            event.getButtonList().add(new GuiButton(888, gui.width / 2 + 5, gui.height / 6 + 108, 150, 20, "Restart SoundHandler"));
         }
     }
     
     @SubscribeEvent
     public void onPostActionPerformed(GuiScreenEvent.ActionPerformedEvent.Post event) {
-        if (event.button.id == 888) {
+        if (event.getButton().id == 888) {
             Minecraft mc = Minecraft.getMinecraft();
             mc.getSoundHandler().onResourceManagerReload(mc.getResourceManager());
         }
